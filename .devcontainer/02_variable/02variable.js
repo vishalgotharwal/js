6b0dcf7770d0d5010a26dc58403def3422202d8e
vishal gotharwal <166741423+vishalgotharwal@users.noreply.github.com>
2024-04-27 14:46:17 +0000

    pretiar

---
+++ .devcontainer/02_variable/02variable.js
@@ -0,0 +1,14 @@
+// alert("vishal") //we are use in node not in browes
+console.log("vishal") 
+console.log(3+3)
+
+
+let name  ="vishal"
+
+
+//number => 2 to power 53
+
+//bigInt 
+
+//string =>""
+//boolean -> true and false
