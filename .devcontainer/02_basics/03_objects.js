fa4d4a48e47c267bf6f153bc9fdbca7a2b496a90
vishal gotharwal <166741423+vishalgotharwal@users.noreply.github.com>
2024-05-04 04:47:25 +0000

    json api and object

---
+++ .devcontainer/02_basics/03_objects.js
@@ -0,0 +1,22 @@
+const course ={
+    coursename : "js in hindi",
+    price :"999",
+    couserinstructor :"hitesh choudhary"
+}
+
+
+
+//A P I -------++++--++-+-+-+-+-+-+-+-+-+-......
+ 
+
+//its treat like a json so key are always in string otherwise they are give error 
+// {
+//    " name ": "vishal",
+//     "age" : 12,
+
+// }
+
+// API in arrys form 
+// [
+//     {},{},{}
+// ]
