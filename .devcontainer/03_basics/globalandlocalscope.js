1773564cc2b65e959b79a3e973c35d2521670357
vishal gotharwal <166741423+vishalgotharwal@users.noreply.github.com>
2024-05-05 15:16:14 +0000

    global and local variable and functions

---
+++ .devcontainer/03_basics/globalandlocalscope.js
@@ -0,0 +1,38 @@
+
+
+// ->local variable = let and const
+
+//->global variable = var
+
+if(true){
+   let a = 10;
+
+    const b = 20;
+    
+    var c = 30;
+}
+
+// console.log(a)
+// console.log(b)
+// console.log(c)
+
+
+function one(){
+    const username = "vishal"
+    two()
+
+    function two(){
+   const website = "youtube"
+       
+        console.log(username)
+         
+    }
+   //console.log(website)
+}
+
+one()
+
+//++++++++++++++-------------++++++++++++++-------$$$$$$$$$$$$$$$$$$$$$$$$
+
+
+
